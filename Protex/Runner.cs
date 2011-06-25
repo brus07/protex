791f23cf4c632e04c9c990498a97ad0d188b4c96
Taras Kushnir <kushnirTV@gmail.com>
2011-06-25 19:31:32 +0300

    Added basic data structures.

---
+++ Protex/Runner.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Protex
+{
+    public class Runner
+    {
+        public Runner()
+        {
+            // TODO implement this constructor later
+        }
+
+        public Runner(RunnerStartInfo startInfo)
+        {
+            // TODO implement this constructor later
+        }
+
+        public Runner(RunnerStartInfo startInfo, RunnerSecurityFlags securityFlags)
+        {
+            // TODO implement this constructor later
+        }
+    }
+}
