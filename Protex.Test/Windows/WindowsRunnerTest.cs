d5673041f2b1e5e536ddd6f51e198f0d2be1359e
brus07 <brus07@gmail.com>
2015-01-02 16:26:44 +0200

    Added first test for runner. Added first variant of WindowsRunner logic.

--- Protex.Test/Windows/WindowsRunnerTest.cs
+++ Protex.Test/Windows/WindowsRunnerTest.cs
@@ -15,6 +15,8 @@ namespace Protex.Test.Windows
         {
             IRunner runner = WindowsCreator.CreateRunner();
             IRunnerStartInfo startInfo = WindowsCreator.CreateRunnerStartInfo();
+            startInfo.ExecutableFile = @"..\..\..\TestData\ExecutableFiles\EmptyExecutable\EmptyExecutable.exe";
+            Assert.IsTrue(System.IO.File.Exists(startInfo.ExecutableFile));
             IResult result = runner.Run(startInfo);
         }
     }
--- Protex/Windows/WindowsRunner.cs
+++ Protex/Windows/WindowsRunner.cs
@@ -1,15 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 
 namespace Protex.Windows
 {
     class WindowsRunner: IRunner
     {
+        private const int UserWorkingTimeKoef = 3;
+
         public IResult Run(IRunnerStartInfo runnerStartInfo)
         {
-            throw new NotImplementedException();
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.CreateNoWindow = false;
+            startInfo.UseShellExecute = false;
+            startInfo.FileName = runnerStartInfo.ExecutableFile;
+            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            startInfo.RedirectStandardOutput = true;
+
+            IResult result = new Result();
+
+            try
+            {
+                Process process = Process.Start(startInfo);
+                DateTime startTime = DateTime.Now;
+                int maximumUserTime = runnerStartInfo.WorkingTimeLimit * UserWorkingTimeKoef;
+                while(process.HasExited == false)
+                {
+                    result.WorkingTime = (int)((DateTime.Now - startTime).TotalMilliseconds);
+                    result.PeakMemoryUsed = Math.Max(result.PeakMemoryUsed, process.PeakVirtualMemorySize64);
+                    if (result.WorkingTime > maximumUserTime)
+                    {
+                        result.WorkingTime = maximumUserTime;
+                        process.Kill();
+                    }
+                    Thread.Sleep(50);
+                }
+                //process.WaitForExit(5000);
+                result.ExitCode = process.ExitCode;
+                process.StandardOutput.ReadToEnd();
+                process.Dispose();
+            }
+            catch
+            {
+                throw;
+            }
+
+            return new Result();
         }
     }
 }
